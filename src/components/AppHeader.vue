05e6a4fd1d9b46e0900660fe511f1da79ad65ef0
Chris Andreotti <chris.andreotti@gmail.com>
2019-11-10 15:26:23 -0500

    Use mapGetters to get isLoggedIn state from auth module

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,18 +3,24 @@
         <a href="/" class="active item">
             Image Storage
         </a>
-    
+
         <div class="right menu">
-            <a href="#" class="ui item" @click="login">Login</a>
+            <div v-if="isLoggedIn">
+                Logged In
+            </div>
+            <a v-else href="#" class="ui item" @click="login">Login</a>
         </div>
     </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapActions, mapGetters } from 'vuex';
 
 export default {
     name: 'AppHeader',
+    computed: {
+        ...mapGetters(['isLoggedIn']),
+    },
     methods: {
         ...mapActions(['login']),
     }
